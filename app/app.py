bbf2410d16493395540b74a563e0aa9ba2b4cafc
Serge Sukhanov <ssuhanov@gmail.com>
2020-05-25 21:11:05 +0300

    Add tags and DB migrations

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 from config import Configuration
 
@@ -7,3 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ def slugify(text):
     return re.sub(pattern, '-', text).lower()
 
 
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
@@ -16,10 +22,11 @@ class Post(db.Model):
     body = db.Column(db.Text)
     created_date = db.Column(db.DateTime, default=datetime.now())
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
-
 
     def generate_slug(self):
         if self.title:
@@ -27,3 +34,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post id: {id}, title: {title}>'.format(id=self.id, title=self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {id}, name: {name}>'.format(id=self.id, name=self.name)
