1013e5818b5d8f3d06099c4c25d569e4520863c3
Serge Sukhanov <ssuhanov@gmail.com>
2020-06-20 13:05:46 +0300

    Add custom 404 page

--- app/view.py
+++ app/view.py
@@ -7,3 +7,8 @@ from app import app
 def index():
     user_name = 'Username'
     return render_template('index.html', name=user_name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
